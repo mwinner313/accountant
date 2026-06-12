d7a75c4da73d4145fcef1fe3362a537f172ed9b3
mohammadali <m.ghanbari01375@gmail.com>
2026-06-12 18:22:41 +0330

    fixed wallet

--- src/Bazaar.Web/src/components/ConfirmHost.vue
+++ src/Bazaar.Web/src/components/ConfirmHost.vue
@@ -2,30 +2,34 @@
 import { useConfirm } from '@/composables/useConfirm'
 import {
   AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle
 } from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 const { open, options, accept, cancel } = useConfirm()
+
+function onOpenChange(value: boolean) {
+  // Escape key dismisses the dialog without clicking our buttons.
+  if (!value && open.value && options.value) cancel()
+}
 </script>
 
 <template>
-  <AlertDialog :open="open" @update:open="v => !v && cancel()">
+  <AlertDialog :open="open" @update:open="onOpenChange">
     <AlertDialogContent>
       <AlertDialogHeader>
         <AlertDialogTitle>{{ t('app.confirm') }}</AlertDialogTitle>
         <AlertDialogDescription>{{ options?.message }}</AlertDialogDescription>
       </AlertDialogHeader>
       <AlertDialogFooter>
-        <AlertDialogCancel @click="cancel">{{ t('app.cancel') }}</AlertDialogCancel>
-        <AlertDialogAction @click="accept">{{ t('app.confirm') }}</AlertDialogAction>
+        <Button type="button" variant="outline" @click="cancel">{{ t('app.cancel') }}</Button>
+        <Button type="button" @click="accept">{{ t('app.confirm') }}</Button>
       </AlertDialogFooter>
     </AlertDialogContent>
   </AlertDialog>
